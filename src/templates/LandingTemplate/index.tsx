e306db40ad0f66a3c9d93d8e316821e1912c0fda
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2024-01-10 15:41:26 +0330

    fix: some problem

--- src/templates/LandingTemplate/index.tsx
+++ src/templates/LandingTemplate/index.tsx
@@ -1,13 +1,5 @@
-import { useTranslations } from 'next-intl'
-
 const LandingTemplate = () => {
-    const t = useTranslations('Landing')
-
-    return (
-        <div className='h-screen grid place-items-center bg-cyan-200'>
-            <span className='text-2xl font-bold text-gray-600'>{t('welcome', { user: 'Get Wrecked' })}</span>
-        </div>
-    )
+    return <div className='h-screen grid place-items-center  '>aa</div>
 }
 
 export { LandingTemplate }
