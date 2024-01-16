dfdec3016f700e02845b6933232e27911c43bbf9
Amirmohamad <56334460+Amirmohamadhejazi@users.noreply.github.com>
2024-01-16 14:19:38 +0330

    fix: internalSidebar

--- src/components/partials/layouts/AppLayout/AppLayout.tsx
+++ src/components/partials/layouts/AppLayout/AppLayout.tsx
@@ -18,3 +18,31 @@ const AppLayout = ({ children }: { children: ReactNode }) => {
 }
 
 export default AppLayout
+
+// import { type ReactNode } from 'react'
+
+// import { InternalSidebar, Sidebar } from './resources'
+
+// const AppLayout = ({ children }: { children: ReactNode }) => {
+//     return (
+//         <div className='flex h-screen   text-[#949ba4] select-none'>
+//             {/* Sidebar */}
+//             {/* <Sidebar /> */}
+
+//             {/* Main Content */}
+//             <div className='flex-auto flex '>
+//                 <div className='flex flex-col h-full w-[130px] bg-cyan-400 p-1'>
+//                     <div className='h-[300px] bg-red-200 p-1'></div>
+//                     <div className='flex grow p-1 bg-slate-500 overflow-auto'>
+//                         <div className='h-[1000px] bg-black p-1'></div>
+//                     </div>
+//                     <div className='h-[200px] bg-blue-200 p-1'>seetubg</div>
+//                 </div>
+//                 {/* <InternalSidebar /> */}
+//                 <div className='flex-auto bg-[#313338]'>{children}</div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default AppLayout
--- src/components/partials/layouts/AppLayout/resources/components/InternalSidebar/InternalSidebar.tsx
+++ src/components/partials/layouts/AppLayout/resources/components/InternalSidebar/InternalSidebar.tsx
@@ -14,7 +14,7 @@ const InternalSidebar = () => {
     const dataSidebar = static_data.find((items) => items.id === sidebarState[2])
 
     return (
-        <div className='w-60 flex flex-col gap-2 bg-[#2b2d31] text-[#80848e] p-[8px]'>
+        <div className='w-60 h-screen    flex flex-col gap-2 bg-[#2b2d31] text-[#80848e] p-[8px]'>
             {sidebarState[2] === 'me' || sidebarState[2] === 'nitro' || sidebarState[2] === 'shop' ? (
                 <div className='flex flex-col px-[4px] py-[6px] gap-2 font-semibold *:py-2'>
                     {static_data_me.map((itemsMe) => (
@@ -61,6 +61,26 @@ const InternalSidebar = () => {
                     <h1 className='text-lg font-bold'>Channel Id is Incorrect!</h1>
                 </div>
             )}
+            {/* <div className='flex grow flex-col px-[10px] '> */}
+            <div className='flex items-center justify-between'>
+                <span className='text-xs'>Direct Messages</span>
+                <span>+</span>
+            </div>
+            {/* <div className='flex flex-col gap-y-2 max-h-[450px] overflow-auto'>
+
+                </div> */}
+            <div className='flex flex-col gap-y-2 grow p-1   overflow-auto'>
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((items) => (
+                    <div className='flex items-center justify-center gap-x-2' key={items}>
+                        <div className='bg-[#313338] p-4 rounded-full ' />
+                        <div className='flex-1 h-2/3 bg-[#313338]  rounded-md'></div>
+                    </div>
+                ))}
+            </div>
+            <div className='flex gap-2 '>
+                <span>seeting</span>
+            </div>
+            {/* </div> */}
         </div>
     )
 }
