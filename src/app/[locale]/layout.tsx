de07319778bbd0562b5a319cba4d684323c44efb
Amirmohamad <56334460+Amirmohamadhejazi@users.noreply.github.com>
2024-03-16 11:42:32 +0330

    fix: icon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,12 +12,20 @@ interface IRootLayoutProps {
     children: ReactNode
     params: { locale: string }
 }
+export const metadata = {
+    icons: {
+        icon: '/icons/discord-icon.webp'
+    }
+}
 
 const RootLayout = ({ children, params: { locale } }: IRootLayoutProps) => {
     if (!LOCALES.includes(locale)) notFound()
 
     return (
         <html lang={locale}>
+            <head>
+                <link rel='icon' href='/icon?<generated>' type='image/png' sizes='32x32' />
+            </head>
             <body className={inter.className}>
                 <RootProvider>{children}</RootProvider>
             </body>
