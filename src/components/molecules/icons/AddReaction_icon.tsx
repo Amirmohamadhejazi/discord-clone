4abe1c477f1e7c915e10acc81e5806ecfeb4046e
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2024-04-03 11:31:13 +0330

    chore: add reaction icon

--- src/components/molecules/icons/AddReaction_icon.tsx
+++ src/components/molecules/icons/AddReaction_icon.tsx
@@ -16,9 +16,9 @@ const AddReaction_icon: FC<IIconProps> = ({ size = '18', className }) => {
         >
             <path
                 fill='currentColor'
-                fill-rule='evenodd'
+                fillRule='evenodd'
                 d='M12 23a11 11 0 1 0 0-22 11 11 0 0 0 0 22ZM6.5 13a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm11 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm-9.8 1.17a1 1 0 0 1 1.39.27 3.5 3.5 0 0 0 5.82 0 1 1 0 0 1 1.66 1.12 5.5 5.5 0 0 1-9.14 0 1 1 0 0 1 .27-1.4Z'
-                clip-rule='evenodd'
+                clipRule='evenodd'
                 className=''
             ></path>
         </svg>
