0693851588475273471951babb2282fb7632bf6b
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2024-04-07 16:08:58 +0330

    fix: bug close modal setting page

--- src/components/partials/layouts/AppLayout/resources/components/Setting/Setting.tsx
+++ src/components/partials/layouts/AppLayout/resources/components/Setting/Setting.tsx
@@ -7,13 +7,13 @@ const Setting = () => {
     const dispatch = useDispatch()
 
     return (
-        <div
-            className='flex w-full h-full bg-general-gray-100   text-general-gray-900'
-            onClick={() => dispatch(closeSettingPage())}
-        >
+        <div className='flex w-full h-full bg-general-gray-100   text-general-gray-900'>
             <div className='w-[350px] bg-general-gray-50 '></div>
             <div className='grow relative'>
-                <div className='absolute text-center top-24 right-24 cursor-pointer'>
+                <div
+                    className='absolute text-center top-24 right-24 cursor-pointer'
+                    onClick={() => dispatch(closeSettingPage())}
+                >
                     <SlClose size={40} />
                     <span className='text-xl'>esc</span>
                 </div>
