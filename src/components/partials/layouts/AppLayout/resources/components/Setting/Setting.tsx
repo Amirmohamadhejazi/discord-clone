370a251a20e85f5af553e34a9f4b73aaf23d1f8e
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2024-04-22 12:50:19 +0330

    chore: setting page

--- src/components/partials/layouts/AppLayout/resources/components/Setting/Setting.tsx
+++ src/components/partials/layouts/AppLayout/resources/components/Setting/Setting.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react'
 import { HiMiniBars3 } from 'react-icons/hi2'
 import { SlClose } from 'react-icons/sl'
 import { useDispatch } from 'react-redux'
-import { ActionIcon } from '@mantine/core'
+import { ActionIcon, CloseIcon } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
 
 import { closeSettingPage } from '@core/services/stores/Reducer/SettingUserPage/SettingUserPageSlice'
 import { type TCriticalAnyType, type TDataSettingSideBar } from '@core/types/common/critical-any'
@@ -17,18 +19,32 @@ const Setting = () => {
         .map((items) => items.sub)
         .flat(Infinity)
         .find((itemSub: TCriticalAnyType) => itemSub.name === sideBar)
+    const mediaMd = useMediaQuery('(max-width: 768px)')
 
+    useEffect(() => {
+        sideBarMobile && setSideBarMobile(false)
+    }, [sideBar])
     return (
         <div className='flex w-full h-full bg-general-gray-100'>
             <div className='w-full flex relative'>
                 <div className='h-full w-1/2 bg-general-gray-50' />
                 <div className='h-full w-1/2 bg-general-gray-100' />
 
-                <div className='flex justify-center size-full absolute top-0 left-1/2 -translate-x-1/2'>
-                    <div className='w-1/3  hidden md:flex justify-end h-full overflow-auto'>
+                <div className='w-full flex justify-center size-full absolute top-0 left-1/2 -translate-x-1/2'>
+                    <div
+                        className={`bg-general-gray-50 absolute md:relative md:flex justify-end  overflow-auto w-full md:w-1/3 h-full z-50 ${
+                            mediaMd && sideBarMobile
+                                ? 'opacity-100 top-0'
+                                : '-top-full md:top-0 opacity-0 md:opacity-100'
+                        } duration-100`}
+                        // className='w-full md:w-1/3  hidden md:flex justify-end h-full overflow-auto'
+                    >
                         <div
-                            className={`w-full md:w-[200px] h-full overflow-auto flex-col gap-2 pr-3  py-14 bg-general-gray-50 shrink-0 flex`}
+                            className={`md:w-[200px] h-full   flex-col gap-2 pr-3  md:py-14 bg-general-gray-50 shrink-0 flex`}
                         >
+                            <div className='flex md:hidden justify-end w-full z-10 bg-general-gray-50 text-white sticky top-0 '>
+                                <CloseIcon size='23' className='my-2 mx-1' onClick={() => setSideBarMobile(false)} />
+                            </div>
                             {(dataSideBar as TDataSettingSideBar).map((item, index) => (
                                 <div className='flex flex-col gap-2' key={index}>
                                     {item.title && (
@@ -61,7 +77,7 @@ const Setting = () => {
                         </div>
                     </div>
                     <div className='w-full md:w-2/3 flex justify-center md:justify-start bg-general-gray-100 h-full overflow-auto'>
-                        <div className='md:max-w-[600px] h-full bg-general-gray-100 text-white  px-8 py-5 md:py-14  '>
+                        <div className='w-full md:max-w-[600px] h-full bg-general-gray-100 text-white  px-8 py-5 md:py-14  '>
                             <div className='flex items-center justify-between relative'>
                                 <ActionIcon
                                     classNames={{ root: 'w-auto  md:hidden' }}
@@ -76,10 +92,16 @@ const Setting = () => {
                                     <SlClose size={33} />
                                 </div>
                             </div>
-                            {`${sideBarMobile}`}
                             {componentSetting?.component && componentSetting.component}
                         </div>
                     </div>
+                    {/* <div
+                            className={`bg-general-gray-100 absolute size-full z-50 ${
+                                sideBarMobile ? 'opacity-100 top-0' : '-top-full opacity-0'
+                            } duration-300`}
+                        >
+                            2
+                        </div> */}
                 </div>
             </div>
         </div>
